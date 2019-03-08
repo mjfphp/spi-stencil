f60940075ff40a324e0931ccc9a094850cb18241
Mjamai <mjf.php@gmail.com>
2019-03-08 16:21:53 +0100

    fix problems

--- src/components/spi-root/spi-root.tsx
+++ src/components/spi-root/spi-root.tsx
@@ -21,6 +21,7 @@ export class SpiRoot {
               <stencil-route url="/profs/:id" component="spi-prof" exact={true} />
               <stencil-route url="/formations" component="spi-formations" exact={true} />
               <stencil-route url="/candidats" component="spi-candidats" exact={true} />
+              <stencil-route url="/candidat/:id" component="spi-candidat" exact={true} />
               <stencil-route url="/candidatAdd/:id" component="spi-candidat-add" exact={true} />
               <stencil-route url="/candidatAdd/" component="spi-candidat-add" exact={true} />
               <stencil-route url="/addFormation" component="spi-formation-add" exact={true} />
